f84a1a82b0c575cafb37b820dabd36706d108f60
Slewog <31140215+Slewog@users.noreply.github.com>
2025-10-31 19:56:03 +0100

    Update get_displey_method
    - Change documentation.
    - Remove unnecessary lines of code

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 from os import path
-import customtkinter as ctk
 
 import const
+import customtkinter as ctk
 
 
 class App(ctk.CTk):
@@ -19,16 +19,19 @@ class App(ctk.CTk):
         self.geometry(self.get_display_center(const.SCREEN_W, const.SCREEN_H))
         self.update() # Just to get the correct values from winfo_width() and winfo_height().
 
-    def get_display_center(self, width: int, height: int) -> str:
-        """Centers the CTK window to the main display/monitor
+    def get_display_center(self, win_width: int, win_height: int) -> str:
+        """Centers the CTK window to the main display.
 
-        :returns: app geometry and position on display
+        Args:
+            win_width = Width of the app window.
+            win_height = Height of the app window.
+
+        Returns:
+            Window geometry.
         """
-        s_width = self.winfo_screenwidth()
-        s_height = self.winfo_screenheight()
-        x = (s_width - width) // 2
-        y = (s_height - height) // 2
-        return f"{width}x{height}+{x}+{y}"
+        x = (self.winfo_screenwidth() - win_width) // 2
+        y = (self.winfo_screenheight() - win_height) // 2
+        return f"{win_width}x{win_height}+{x}+{y}"
 
 if __name__ == '__main__':
     app = App()
