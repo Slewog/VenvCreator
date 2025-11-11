003f33ebd310834eec41d8dd65caf0c06d570ffe
Slewog <31140215+Slewog@users.noreply.github.com>
2025-11-11 17:58:29 +0100

    Add font and header parameters

--- const.py
+++ const.py
@@ -3,3 +3,18 @@ SCREEN_H = 460
 SCREEN_RESIZABLE = False
 SCREEN_TITLE = 'Virtual Environment Creator'
 SCREEN_ICON = 'python.ico'
+
+FONTS = {
+    "family": "Segoe UI",
+    "weight": "bold",
+    "slant": "roman", 
+    "size": 15
+}
+
+HEADER = {
+    'text': 'Create a Python virtual environment:',
+    'text_pady': (5, 0),
+    'img_path': 'python.png',
+    'img_size': (150, 150),
+    'pady': (15, 20)
+}
