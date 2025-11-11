6c56cb54ca9c7aa48e7ccab2b589d8930074f9b6
Slewog <31140215+Slewog@users.noreply.github.com>
2025-11-11 17:59:27 +0100

    Create header frame
    Set default theme to dark.
    Create header frame.

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import sys
 from os import path
+from PIL import Image
 
 import const
 import customtkinter as ctk
 
+ctk.set_appearance_mode('dark')
+
+
+class HeaderFrame(ctk.CTkFrame):
+    def __init__(self, master: ctk.CTk, current_path: str):
+        super().__init__(master)
+
+        tmp_img = Image.open(path.join(current_path, "assets", const.HEADER['img_path']))
+
+        head_logo = ctk.CTkLabel(
+            self,
+            image=ctk.CTkImage(
+                light_image=tmp_img,
+                dark_image=tmp_img,
+                size=const.HEADER['img_size']
+            ),
+            text="" # Stay empty.
+        )
+        head_logo.grid(column=0, row=0)
+
+        head_label = ctk.CTkLabel(
+            self,
+            text=const.HEADER['text'],
+            font=(const.FONTS['family'], const.FONTS['size'], const.FONTS['weight'], const.FONTS['slant'])
+        )
+        head_label.grid(column=0, row=1, pady=const.HEADER['text_pady'])
+
+        self.grid(column=0, row=0, pady=const.HEADER['pady'])
+
 
 class App(ctk.CTk):
-    def __init__(self, fg_color = None, **kwargs):
-        super().__init__(fg_color, **kwargs)
+    def __init__(self):
+        super().__init__()
         self.bind('<Escape>', lambda e: self.destroy()) # Bind Escape key to close the app.
 
         # Get absolute path, works for dev and for PyInstaller.
@@ -17,6 +47,11 @@ class App(ctk.CTk):
         self.resizable(width=const.SCREEN_RESIZABLE, height=const.SCREEN_RESIZABLE)
         self.iconbitmap(path.join(self.current_path, "assets", const.SCREEN_ICON))
         self.geometry(self.get_display_center(const.SCREEN_W, const.SCREEN_H))
+
+        self.grid_columnconfigure(0, weight=1)
+
+        self.head_frame = HeaderFrame(master=self, current_path=self.current_path)
+
         self.update() # Just to get the correct values from winfo_width() and winfo_height().
 
     def get_display_center(self, win_width: int, win_height: int) -> str:
@@ -32,6 +67,7 @@ class App(ctk.CTk):
         x = (self.winfo_screenwidth() - win_width) // 2
         y = (self.winfo_screenheight() - win_height) // 2
         return f"{win_width}x{win_height}+{x}+{y}"
+
 
 if __name__ == '__main__':
     app = App()
