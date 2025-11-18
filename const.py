eb999b3e7a9c7e167432dc0dc4d05dd29e51ddd2
Slewog <31140215+Slewog@users.noreply.github.com>
2025-11-19 00:52:27 +0100

    Create widgets, remove unnecessary CONST

--- const.py
+++ const.py
@@ -5,17 +5,6 @@ SCREEN_TITLE = 'Virtual Environment Creator'
 SCREEN_ICON = 'python.ico'
 APPEARANCE_MODE = 'dark'
 
-FONTS = {
-    "family": "Segoe UI",
-    "weight": "bold",
-    "slant": "roman", 
-    "size": 15
-}
-
-HEADER = {
-    'text': 'Create a Python virtual environment:',
-    'text_pady': (5, 0),
-    'img_path': 'python.png',
-    'img_size': (150, 150),
-    'pady': (15, 20)
-}
+FRAME_COLORS = 'transparent'
+ENTRY_WIDTH = 300
+FONT_FAMILY = 'Segoe UI'
